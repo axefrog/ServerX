c8998f761c74bdbfecbad6f7ece954680e31953f
Nathan Ridley <axefrog@gmail.com>
2011-07-03 13:53:01 +0100

    set 'Any CPU' build as default

--- ServerX/ScriptRunner.cs
+++ ServerX/ScriptRunner.cs
@@ -1,33 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using IronJS.Hosting;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using IronJS.Hosting;
 
-namespace ServerX
-{
-	/* 
-	 */
-	public class ScriptManager
-	{
-		private CSharp.Context _js;
+//namespace ServerX
+//{
+//    /* 
+//     */
+//    public class ScriptManager
+//    {
+//        private CSharp.Context _js;
 
-		public ScriptManager()
-		{
-			_js = new CSharp.Context();
-			//IronJS.Native.Utils.CreateFunction(_js.Environment, 
-			_js.Execute("a = 25;");
-		}
+//        public ScriptManager()
+//        {
+//            _js = new CSharp.Context();
+//            //IronJS.Native.Utils.CreateFunction(_js.Environment, 
+//            _js.Execute("a = 25;");
+//        }
 
-		public void ExecuteJavaScript(string js)
-		{
+//        public void ExecuteJavaScript(string js)
+//        {
 			
-		}
-	}
+//        }
+//    }
 
-	public enum ScriptType
-	{
-		CommandList,
-		JavaScript
-	}
-}
+//    public enum ScriptType
+//    {
+//        CommandList,
+//        JavaScript
+//    }
+//}
